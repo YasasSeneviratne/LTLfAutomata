eb86e5caea6ac0bf57533a6f00c14f9295b091b0
Tommy Tracy II <tjt7a@virginia.edu>
2020-02-07 01:32:05 -0500

    Added code to have APSim iterate through a directory of ANML files to convert into HDL

--- Code/APSim/APSim.py
+++ Code/APSim/APSim.py
@@ -6,6 +6,7 @@ import shutil
 from automata.utility.utility import minimize_automata
 import automata.HDL.hdl_generator as hd_gen
 from multiprocessing.dummy import Pool as ThreadPool
+import glob
 
 out_dir_prefix = './'
 dbw = 4
@@ -13,75 +14,69 @@ dbw = 4
 # Get the usage string
 def usage():
     usage = "----------------- Usage ----------------\n"
-    usage += "./APSim.py <input ANML>"
+    usage += "./APSim.py <input ANML directory> <output HDL directory>"
     return usage
-
-
-def generate_hardware(atm):
-
-    return_result = {}
-    result_dir = out_dir_prefix + str(atm)
-
-    # Clean up directory
-    shutil.rmtree(result_dir, ignore_errors=True)
-    os.mkdir(result_dir)
-
-    # Create a diectory name for the HDL code
-    hdl_folder_name = hd_gen.get_hdl_folder_name(prefix=str(atm), number_of_atms=1,
-                                                 stride_value=0, before_match_reg=False,
-                                                 after_match_reg=False, ste_type=1, use_bram=False,
-                                                 use_compression=False, compression_depth=-1)
-    
-    print("Folder name to store the HDLs: ", hdl_folder_name)
-
-    generator_ins = hd_gen.HDL_Gen(path=os.path.join(result_dir, hdl_folder_name), before_match_reg=False,
-                                   after_match_reg=False, ste_type=1,
-                                   total_input_len=dbw)
-    
-    b_atm = atma.automata_network.get_bit_automaton(atm, original_bit_width=atm.max_val_dim_bits_len)
-    atm = atma.automata_network.get_strided_automata2(atm=b_atm,
-                                                      stride_value=dbw,
-                                                      is_scalar=True,
-                                                      base_value=2,
-                                                      add_residual=True)
-    atm.make_homogenous(plus_src=True)
-    minimize_automata(atm)
-
-    generator_ins.register_automata(atm=atm, use_compression=False)
-
-    generator_ins.register_stage_pending(single_out=False, use_bram=False)
-
-    generator_ins.finilize()
-    shutil.make_archive(hdl_folder_name, 'zip', result_dir)
-    shutil.rmtree(result_dir)
 
 
 if __name__ == '__main__':
     
-    verbose = True
-
     # Check the correct number of command line arguments
     if len(sys.argv) != 3:
         print(usage())
         exit(-1)
     
-    # ANML input file
-    anml_input_file = sys.argv[1]
+    # ANML directory
+    input_anml_directory = sys.argv[1]
+    output_hdl_directory = sys.argv[2]
 
-    # Parse the ANML file
-    automatas = atma.parse_anml_file(anml_input_file)
+    anml_input_files = glob.glob(input_anml_directory + '/*.anml')
 
-    automatas.draw_graph("original_non_homo.svg")
+    # Clean up directory
+    shutil.rmtree(output_hdl_directory, ignore_errors=True)
+    os.mkdir(output_hdl_directory)
 
-    automatas.make_homogenous(plus_src=True)
-    #automatas = automatas.get_single_stride_graph()
+    # Create a directory name for the HDL code
+    hdl_folder_name = hd_gen.get_hdl_folder_name(prefix='temp', number_of_atms=len(anml_input_files),
+                                                stride_value=0, before_match_reg=False,
+                                                after_match_reg=False, ste_type=1, use_bram=False,
+                                                use_compression=False, compression_depth=-1)
+                    
+    print("Folder name to store the HDLs: ", hdl_folder_name)
 
-    automatas.draw_graph("original_homo.svg")
+    generator_ins = hd_gen.HDL_Gen(path=os.path.join(output_hdl_directory, hdl_folder_name), before_match_reg=False,
+                                   after_match_reg=False, ste_type=1,
+                                   total_input_len=dbw)
 
-    minimize_automata(automatas)
+    # Iterate through the ANML files in the directory
+    for index, anml_input_file in enumerate(anml_input_files):
 
-    print(automatas.get_summary(logo=" of the homogeneous, minimized, automata"))
+        # Parse the ANML file
+        automata = atma.parse_anml_file(anml_input_file)
 
-    automatas.draw_graph("original_homo_min.svg")
+        # Assign each unique automaton its own unique name or the HDL generator won't work
+        automata.id = 'an{}'.format(index)
 
-    generate_hardware(automatas)
+        # Convert to homogeneous and minimized form
+        automata.make_homogenous()
+        minimize_automata(automata)
+
+        print(automata.get_summary(logo=" of the homogeneous, minimized, automata"))
+
+        # Convert to a dbw-bitwidth automaton
+        b_atm = atma.automata_network.get_bit_automaton(automata, original_bit_width=automata.max_val_dim_bits_len)
+        automata = atma.automata_network.get_strided_automata2(atm=b_atm,
+                                                      stride_value=dbw,
+                                                      is_scalar=True,
+                                                      base_value=2,
+                                                      add_residual=True)
+        automata.make_homogenous()
+        minimize_automata(automata)
+        generator_ins.register_automata(atm=automata, use_compression=False)
+
+    # Once all of the automata have been registered, add registers
+    generator_ins.register_stage_pending(single_out=False, use_bram=False)
+
+    #Finalize and wrap up HDL in archive folder
+    generator_ins.finilize()
+    shutil.make_archive(hdl_folder_name, 'zip', output_hdl_directory)
+    shutil.rmtree(output_hdl_directory)
