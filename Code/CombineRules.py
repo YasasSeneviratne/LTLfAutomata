e5f8132d93a4d7d3aaac4eb61f4186d534e4934e
lucasmt <lmartinellitabajara@gmail.com>
2020-03-10 17:49:42 -0400

    Updated scripts for combining rules to create more complex rules from patterns
    Complex rules are formed by taking the conjunction of individual patterns

--- Code/CombineRules.py
+++ Code/CombineRules.py
@@ -16,48 +16,57 @@ import re
 # Get the usage string
 def usage():
     usage = "----------------- Usage ----------------\n"
-    usage += "./CombineRules.py <random seed> <number of rules> <number of variables> <output directory> *<rule files>"
+    usage += "./CombineRules.py <random seed> <number of rules> <number of patterns per rule> <number of variables> <output format> *<pattern files>"
     return usage
 
-def generate_rule(rule_file, number_of_variables, output_file):
-    with open(rule_file, 'r') as file:
-        with open(output_file, 'w') as out:
+def generate_rule(samples, new_vars, output_file):
+    vars_used = set()
+    conjuncts = []
+
+    for rule_file in samples:
+        with open(rule_file, 'r') as file:
             line = file.readline()
 
-            while line and not line.startswith("DFA for formula with free variables:"):
-                out.write(line)
+            while line and not line.startswith("var2"):
                 line = file.readline()
 
-            old_vars = line.rstrip().split(": ")[1].split(" ")
-            new_vars = ['I%d' % i for i in range(number_of_variables)]
-            
+            # line is the list of variables
+
+            # remove trailing whitespace and semicolon, then split on spaces and discard first token ("var2")
+            old_vars = line.rstrip().rstrip(";").replace(",", "").split(" ")[1:]
             new_var_sample = random.sample(new_vars, len(old_vars))
+            vars_used = vars_used | set(new_var_sample)
+            var_dict = dict(zip(old_vars, new_var_sample))
 
-            out.write("DFA for formula with free variables: " + " ".join(new_var_sample) + "\n")
+            formula = file.readline().rstrip().rstrip(";")
+            pattern = re.compile("|".join(var_dict))
+            conjuncts.append(pattern.sub(lambda m: var_dict[m.group(0)], formula))
 
-            line = file.readline()
-            
-            while line:
-                out.write(line)
-                line = file.readline()
+    with open(output_file, 'w') as out:
+        out.write("m2l-str;\n")
+        out.write("var2 " + ", ".join(vars_used) + ";\n")
+        out.write(" & ".join(conjuncts) + ";\n")
 
 # Entry point
 if __name__ == '__main__':
 
     # Check the correct number of command line arguments
-    if len(sys.argv) < 5:
+    if len(sys.argv) < 6:
         print(usage())
         exit(-1)
 
     random_seed = int(sys.argv[1])
     number_of_rules = int(sys.argv[2])
-    number_of_variables = int(sys.argv[3])
-    output_directory = sys.argv[4]
-    rule_files = sys.argv[5:]
+    number_of_patterns_per_rule = int(sys.argv[3])
+    number_of_variables = int(sys.argv[4])
+    output_format = sys.argv[5]
+    pattern_files = sys.argv[6:]
 
     random.seed(random_seed)
     
     for i in range(number_of_rules):
         print("Rule %d" % i)
-        rule_file = rule_files[random.randrange(len(rule_files))]
-        generate_rule(rule_file, number_of_variables, output_directory + '/rule' + str(i) + '.out')
+        samples = [ pattern_files[random.randrange(len(pattern_files))] for _ in range(number_of_patterns_per_rule) ]
+        new_vars = ['I%d' % i for i in range(number_of_variables)]
+        output_file = output_format % i
+        generate_rule(samples, new_vars, output_file)
