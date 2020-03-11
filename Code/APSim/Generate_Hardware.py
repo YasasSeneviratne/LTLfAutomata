3e18ce605bb39be20412071ef7ba8c8badf2fa25
Tommy Tracy II <tjt7a@virginia.edu>
2020-03-11 17:49:54 -0400

    Pushing in-development stuff to merge with Lucas's latest

--- Code/APSim/Generate_Hardware.py
+++ Code/APSim/Generate_Hardware.py
@@ -13,7 +13,6 @@ import sys, os
 import shutil
 from automata.utility.utility import minimize_automata
 import automata.HDL.hdl_generator as hd_gen
-from multiprocessing.dummy import Pool as ThreadPool
 import glob
 
 dbw = None
@@ -21,91 +20,159 @@ dbw = None
 # Get the usage string
 def usage():
     usage = "----------------- Usage ----------------\n"
-    usage += "./APSim.py <automata symbol bit width> <input ANML directory> <output HDL directory>"
+    usage += "./APSim.py <automata symbol bit width> <input ANML file directory> <automata per stage> [--symbolic]"
     return usage
 
-
-if __name__ == '__main__':
-    
-    # Check the correct number of command line arguments
-    if len(sys.argv) != 4:
-        print(usage())
-        exit(-1)
-    
-    # Bitwidth of the explicit automata
-    dbw = int(sys.argv[1])
-
-    # This is the directory that contains the ANML files
-    input_anml_directory = sys.argv[2]
+# Process ANML
+def process_anml(bitwidth, input_anml_directory, automata_per_stage):
 
     # This is the directory name to be created for HDL files
-    output_hdl_directory = sys.argv[3]
+    output_hdl_directory = input_anml_directory + '_' + str(bitwidth) + '_' + str(automata_per_stage)
 
     anml_input_files = glob.glob(input_anml_directory + '/*.anml')
+    print "ANML Files: ", anml_input_files
 
-    print "input files: ", anml_input_files
+    # Parse the ANML file
+    automata = atma.parse_anml_file(anml_input_file)
+
+    # Minimizing the automata with NFA heuristics
+    print "Minimizing Automata"
+    minimize_automata(automata)
 
     # Clean up directory
     shutil.rmtree(output_hdl_directory, ignore_errors=True)
     os.mkdir(output_hdl_directory)
 
     # Create a directory name for the HDL code
-    hdl_folder_name = hd_gen.get_hdl_folder_name(prefix='temp', number_of_atms=len(anml_input_files),
-                                                stride_value=dbw, before_match_reg=False,
+    hdl_folder_name = hd_gen.get_hdl_folder_name(prefix=output_hdl_directory, number_of_atms=len(anml_input_files),
+                                                stride_value=0, before_match_reg=False,
                                                 after_match_reg=False, ste_type=1, use_bram=False,
-                                                use_compression=False, compression_depth=-1)
+                                                use_compression=False, compression_depth=-1, symbolic=False)
                     
     print "Folder name to store the HDLs: ", hdl_folder_name
 
     # Create a hardware Generator
     generator_ins = hd_gen.HDL_Gen(path=os.path.join(output_hdl_directory, hdl_folder_name), before_match_reg=False,
                                    after_match_reg=False, ste_type=1,
-                                   total_input_len=dbw)
+                                   total_input_len=dbw, symbolic=False)
+
+
 
     # Iterate through the ANML files in the directory
     for index, anml_input_file in enumerate(anml_input_files):
 
-        print "ANML Input File: ", anml_input_file
-
-        # Parse the ANML file
-        automata = atma.parse_anml_file(anml_input_file)
-
         # Assign each unique automaton its own unique name or the HDL generator won't work
         automata.id = 'an{}'.format(index)
 
-        print "The automata {} homogeneous!".format('is' if automata.is_homogeneous else 'is not')
-
-        if not automata.is_homogeneous:
-            print "Making automata homogeneous"
-            
-            # Convert to homogeneous and minimized form
-            automata.make_homogenous()
-
-        print "Minimizing Automata"
-        print automata.get_summary(logo=" of the homogeneous, automata")
-        minimize_automata(automata)
-        print automata.get_summary(logo=" of the homogeneous, minimized, automata")
-
-        # Convert to a dbw-bitwidth automaton
-        # b_atm = atma.automata_network.get_bit_automaton(automata, original_bit_width=automata.max_val_dim_bits_len)
-        # automata = atma.automata_network.get_strided_automata2(atm=b_atm,
-        #                                               stride_value=dbw,
-        #                                               is_scalar=True,
-        #                                               base_value=2,
-        #                                               add_residual=True)
-        # automata.make_homogenous()
-        # minimize_automata(automata)
-
         # Drawing automata graph
         print "Drawing automata svg graph"
-        automata.draw_graph(anml_input_file + "_minimized_HW.svg")
+        automata.draw_graph(anml_input_file + "_minimized_hw.svg")
 
+        # Register this automaton
         generator_ins.register_automata(atm=automata, use_compression=False)
 
-    # Once all of the automata have been registered, add registers
+        # We've got another batch of automata_per_stage automata to stage
+        if (index + 1) % automata_per_stage == 0:
+            generator_ins.register_stage_pending(single_out=False, use_bram=False)
+
+    # DO we need this? maybe if our number of automata is not a perfect multiple
+    # of automata_per_stage?
     generator_ins.register_stage_pending(single_out=False, use_bram=False)
 
     #Finalize and wrap up HDL in archive folder
     generator_ins.finilize()
-    shutil.make_archive(hdl_folder_name, 'zip', output_hdl_directory)
+
+    # Using gztar to handle LARGE automata workloads
+    shutil.make_archive(hdl_folder_name, 'gztar', output_hdl_directory)
     shutil.rmtree(output_hdl_directory)
+
+
+def process_truthtable(bitwidth, input_anml_directory, automata_per_stage):
+
+    # This is the directory name to be created for HDL files
+    output_hdl_directory = input_anml_directory + '_' + str(bitwidth) + '_' + str(automata_per_stage)
+
+    # Grab the input files
+    truthtable_input_files = glob.glob(input_anml_directory + '/*.tt')
+    print "Truth Table Files: ", truthtable_input_files
+
+    # Clean up directory
+    shutil.rmtree(output_hdl_directory, ignore_errors=True)
+    os.mkdir(output_hdl_directory)
+
+    # Create a directory name for the HDL code
+    hdl_folder_name = hd_gen.get_hdl_folder_name(prefix=output_hdl_directory, number_of_atms=len(truthtable_input_files),
+                                                stride_value=0, before_match_reg=False,
+                                                after_match_reg=False, ste_type=1, use_bram=False,
+                                                use_compression=False, compression_depth=-1, symbolic=True)
+                    
+    print "Folder name to store the HDLs: ", hdl_folder_name
+
+    # Create a hardware Generator
+    generator_ins = hd_gen.HDL_Gen(path=os.path.join(output_hdl_directory, hdl_folder_name), before_match_reg=False,
+                                   after_match_reg=False, ste_type=1,
+                                   total_input_len=dbw, symbolic=True)
+
+
+    # Iterate through the ANML files in the directory
+    for index, anml_input_file in enumerate(truthtable_input_files):
+
+        # Assign each unique automaton its own unique name or the HDL generator won't work
+        automata.id = 'an{}'.format(index)
+
+        # Drawing automata graph
+        print "Drawing automata svg graph"
+        automata.draw_graph(anml_input_file + "_minimized_hw.svg")
+
+        # Register this automaton
+        generator_ins.register_automata(atm=automata, use_compression=False)
+
+        # We've got another batch of automata_per_stage automata to stage
+        if (index + 1) % automata_per_stage == 0:
+            generator_ins.register_stage_pending(single_out=False, use_bram=False)
+
+    # DO we need this? maybe if our number of automata is not a perfect multiple
+    # of automata_per_stage?
+    generator_ins.register_stage_pending(single_out=False, use_bram=False)
+
+    #Finalize and wrap up HDL in archive folder
+    generator_ins.finilize()
+
+    # Using gztar to handle LARGE automata workloads
+    shutil.make_archive(hdl_folder_name, 'gztar', output_hdl_directory)
+    shutil.rmtree(output_hdl_directory)
+
+
+if __name__ == '__main__':
+    
+    # Check the correct number of command line arguments
+    if len(sys.argv) == 4:
+        symbolic = False
+    elif len(sys.argv) == 5 and sys.argv[4] == "--symbolic":
+        symbolic = True
+    else:
+        print(usage())
+        exit(-1)
+    
+    # Bitwidth of the explicit automata
+    if sys.argv[1].isdigit():
+        dbw = int(sys.argv[1])
+    else:
+        print "Error with first argument: {}; should be a positive integer".format(sys.argv[1])
+        exit(2)
+
+    # This is the directory that contains the ANML files
+    input_anml_directory = sys.argv[2]
+
+    # This is the number of automata set per stage in the pipeline
+    if sys.argv[3].isdigit():
+        automata_per_stage = int(sys.argv[3])
+    else:
+        print "Error with third argument: {}; should be a positive integer".format(sys.argv[3])
+        exit(2)
+
+    # Process either Truth Tables or ANML files
+    if symbolic:
+        process_truthtable(bdw, input_anml_directory, automata_per_stage)
+    else:
+        process_anml(bdw, input_anml_directory, automata_per_stage)
--- Examples/mona_outputs/make-anml-from-rules.sh
+++ Examples/mona_outputs/make-anml-from-rules.sh
@@ -5,18 +5,63 @@
 # This is based on LTLfAutomata/code/make-anml-from-rules.sh
 
 for directory in ./patterns/*/; do
-	echo $directory	
+	# Move into pattern directory
 	cd $directory
-	rm *.anml *.txt *.svg *num_states
+	# Delete old junk
+	rm -f *.anml *.txt *.svg *num_states *.zip *.tar.gz
 	for file in *.out; do 	
-		~/src/LTLfAutomata/Code/MONAtoDFA.py $file $file.anml | grep num_states > $file.num_states
-		echo $file
-		cat $file.num_states
+		# Automatize and grab number of states in non-homogeneous
+		#~/src/LTLfAutomata/Code/MONAtoDFA.py $file $file.anml | grep num_states > $file.nh.num_states
+		nhnumstates=$(cat $file.nh.num_states)
+		nhnumstates="${nhnumstates#*:}"
+		# Get number of state transition elements in homogeneous version
+		hnumstates=$(~/src/VASim/vasim $file.anml | grep -m 1 Elements:)
+		hnumstates="${hnumstates#*:}"
+		# Get number of states from minimized homogenous version
+		#python ~/src/LTLfAutomata/Code/APSim/Minimize_and_homogenize.py $file.anml
+		mhnumstates=$(~/src/VASim/vasim $file.anml_min.anml | grep -m 1 Elements:)
+		mhnumstates="${mhnumstates#*:}"
+		# Get number of signals
+		numsignals=$(cat $file | grep "DFA for formula with free variables:")
+		numsignals="${numsignals#*:}"
+		numsignals=$(echo $numsignals | wc -w)
+		echo $file, $nhnumstates, $hnumstates, $mhnumstates, $numsignals
+
+		# Generate hardware
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 1
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 10
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 100
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 1000
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 10000
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 100000
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 1000000
 	done
 	for file in *.rout; do 	
-		~/src/LTLfAutomata/Code/MONAtoDFA.py $file $file.anml --reverse | grep num_states > $file.num_states
-		echo $file
-		cat $file.num_states
+		# Automatize and grab number of states in non-homogeneous
+		#~/src/LTLfAutomata/Code/MONAtoDFA.py $file $file.anml --reverse | grep num_states > $file.nh.num_states
+		nhnumstates=$(cat $file.nh.num_states)
+		nhnumstates="${nhnumstates#*:}"
+		# Get number of state transition elements in homogeneous version
+		hnumstates=$(~/src/VASim/vasim $file.anml | grep -m 1 Elements:)
+		hnumstates="${hnumstates#*:}"
+		# Get number of states from minimized homogenous version
+		#python ~/src/LTLfAutomata/Code/APSim/Minimize_and_homogenize.py $file.anml
+		mhnumstates=$(~/src/VASim/vasim $file.anml_min.anml | grep -m 1 Elements:)
+		mhnumstates="${mhnumstates#*:}"
+		# Get number of signals
+		numsignals=$(cat $file | grep "DFA for formula with free variables:")
+		numsignals="${numsignals#*:}"
+		numsignals=$(echo $numsignals | wc -w)
+		echo $file, $nhnumstates, $hnumstates, $mhnumstates, $numsignals
+
+		# Generate hardware
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 1
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 10
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 100
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 1000
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 10000
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 100000
+		#~/src/LTLfAutomata/Code/APSim/Generate_Single_Hardware.py $numsignals $file.anml_min.anml 1000000
 	done
 	cd ../..
 done
