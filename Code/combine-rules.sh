48d05648913dde9a092fa98fbe8b39d50c31221f
lucasmt <lmartinellitabajara@gmail.com>
2020-03-12 14:37:34 -0400

    Updated combine-rules.sh to call CombineRules.py for the reverse formulas as well
    combine-rules.sh calls CombineRules.py for both the original formulas and reverse formulas with the same seed

--- Code/combine-rules.sh
+++ Code/combine-rules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 number_of_rules=100
-number_of_patterns_per_rule=10
+number_of_patterns_per_rule=3
 number_of_vars=10
 
 input_dir="../Examples/mona_inputs/patterns"
@@ -12,16 +12,25 @@ mkdir -p ${output_dir}
 mkdir -p ${dfa_dir}
 
 # All files in this format will be used as candidate patterns to sample from
-input_format="${input_dir}/*/*.fol"
+input_prefix="${input_dir}/*/*"
+input_format="${input_prefix}.fol"
+reverse_input_format="${input_prefix}.rfol"
+
 # Combined formulas and automata will be in this format, with %d replaced by numbers between 0 and number_of_rules - 1
-output_format="${output_dir}/rule%d.fol"
-dfa_format="${dfa_dir}/rule%d.out"
+output_prefix="${output_dir}/rule%d"
+output_format="${output_prefix}.fol"
+reverse_output_format="${output_prefix}.rfol"
+dfa_prefix="${dfa_dir}/rule%d"
+dfa_format="${dfa_prefix}.out"
+reverse_dfa_format="${dfa_prefix}.rout"
 
 seed=${RANDOM}
 
 echo "Generating rules (random seed: ${seed})..."
 
 python3 CombineRules.py ${seed} ${number_of_rules} ${number_of_patterns_per_rule} ${number_of_vars} ${output_format} ${input_format}
+
+python3 CombineRules.py ${seed} ${number_of_rules} ${number_of_patterns_per_rule} ${number_of_vars} ${reverse_output_format} ${reverse_input_format}
 
 echo "Constructing DFAs..."
 
@@ -31,6 +40,11 @@ do
     output_file=$(printf "${output_format}" ${i})
     dfa_file=$(printf "${dfa_format}" ${i})
     mona -xw -u ${output_file} > ${dfa_file}
+
+    echo "Reverse DFA for rule ${i}..."
+    reverse_output_file=$(printf "${reverse_output_format}" ${i})
+    reverse_dfa_file=$(printf "${reverse_dfa_format}" ${i})
+    mona -xw -u ${reverse_output_file} > ${reverse_dfa_file}
 done
 
 echo "Done!"
