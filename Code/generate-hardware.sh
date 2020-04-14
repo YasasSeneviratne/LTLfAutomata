28d812a85cd5941e406596e6a6b5aeeb88c3cf8a
Tommy Tracy II <tjt7a@virginia.edu>
2020-04-14 16:05:17 -0400

    Parallelized hardware generation

--- Code/generate-hardware.sh
+++ Code/generate-hardware.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-symbol_bit_width=10
+symbol_bit_width=100
 
 for num_automata in 10 100 1000 10000
 do
@@ -11,9 +11,23 @@ do
     tt_dir="../Examples/mona_outputs/combined_${num_automata}/tt"
     rtt_dir="../Examples/mona_outputs/combined_${num_automata}/rtt"
 
-    ~/src/LTLfAutomata/Code/APSim/Generate_Hardware.py ${symbol_bit_width} ${anml_dir} ${num_automata}
-    ~/src/LTLfAutomata/Code/APSim/Generate_Hardware.py ${symbol_bit_width} ${ranml_dir} ${num_automata}
-    ~/src/LTLfAutomata/Code/APSim/Generate_Hardware.py ${symbol_bit_width} ${tt_dir} ${num_automata} --symbolic
-    ~/src/LTLfAutomata/Code/APSim/Generate_Hardware.py ${symbol_bit_width} ${rtt_dir} ${num_automata} --symbolic
+    explicittask(){
+        echo "Running Explicit Hardware Generator"
+        ~/src/LTLfAutomata/Code/APSim/Generate_Hardware.py $1 $2 $3
+    }
+
+    symbolictask(){
+        echo "Running Symbolic Hardware Generator"
+        ~/src/LTLfAutomata/Code/APSim/Generate_Hardware.py $1 $2 $3 --symbolic
+    }
+
+    explicittask ${symbol_bit_width} ${anml_dir} ${num_automata} &
+    explicittask ${symbol_bit_width} ${ranml_dir} ${num_automata} &
+
+    symbolictask ${symbol_bit_width} ${tt_dir} ${num_automata} &
+    symbolictask ${symbol_bit_width} ${rtt_dir} ${num_automata} &
+
+    wait
+    echo "Done running Harware Generators"
 
 done
