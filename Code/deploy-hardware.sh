dfe2546d4af07c6399fd964bcf302d4f0414e80f
Tommy Tracy II <tjt7a@virginia.edu>
2020-04-14 16:06:11 -0400

    Updated deployment script to handle a variable number of variables

--- Code/deploy-hardware.sh
+++ Code/deploy-hardware.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-target1="18.208.225.41"
-target2="34.235.26.149"
+target1="52.91.197.145" # for dfas
+target2="34.235.26.149" #for tts
+target3="54.227.120.68" #for rtts
+target4="34.234.83.253" #for nfas
 
-for num_automata in 10 100 1000 10000
+number_of_vars=100
+
+for num_automata in 10 100 #1000 10000
 do
     echo "Generating ${automata} automata"
 
@@ -13,20 +17,20 @@ do
     rtt_dir="../rtt"
 
     cd ${anml_dir}
-    zip -r ${num_automata}.zip 10_*
+    zip -r ${num_automata}.zip ${number_of_vars}_*
     scp -i ~/Documents/aws/AWS_Automata_RTL.pem ${num_automata}.zip centos@${target1}:~/Downloads/dfas/.
 
     cd ${ranml_dir}
-    zip -r ${num_automata}.zip 10_*
-    scp -i ~/Documents/aws/AWS_Automata_RTL.pem ${num_automata}.zip centos@${target1}:~/Downloads/nfas/.
+    zip -r ${num_automata}.zip ${number_of_vars}_*
+    scp -i ~/Documents/aws/AWS_Automata_RTL.pem ${num_automata}.zip centos@${target4}:~/Downloads/nfas/.
 
     cd ${tt_dir}
-    zip -r ${num_automata}.zip 10_*
+    zip -r ${num_automata}.zip ${number_of_vars}_*
     scp -i ~/Documents/aws/AWS_Automata_RTL.pem ${num_automata}.zip centos@${target2}:~/Downloads/tts/.
 
     cd ${rtt_dir}
-    zip -r ${num_automata}.zip 10_*
-    scp -i ~/Documents/aws/AWS_Automata_RTL.pem ${num_automata}.zip centos@${target2}:~/Downloads/rtts/.
+    zip -r ${num_automata}.zip ${number_of_vars}_*
+    scp -i ~/Documents/aws/AWS_Automata_RTL.pem ${num_automata}.zip centos@${target3}:~/Downloads/rtts/.
 
     cd ../../../../Code
 
