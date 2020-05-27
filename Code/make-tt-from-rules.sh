52168f42a0f8d600f50ef3379dad9cf92459529e
Tommy Tracy II <tjt7a@virginia.edu>
2020-05-26 21:16:28 -0400

    Fixed missing reverse parameter

--- Code/make-tt-from-rules.sh
+++ Code/make-tt-from-rules.sh
@@ -2,23 +2,24 @@
 
 #Comment: This script has been multi-threaded; make sure the below variables are set correctly
 
+threads=10
+
 for num_automata in 10 100 1000 10000
 do
     input_dir="../Examples/mona_outputs/combined_${num_automata}"
-    
     anml_dir="../Examples/mona_outputs/combined_${num_automata}/tt"
     ranml_dir="../Examples/mona_outputs/combined_${num_automata}/rtt"
-    threads=10
 
     mkdir -p ${anml_dir}
     mkdir -p ${ranml_dir}
 
+    # The task is responsible for generating truthtable files from out and rout MONA outputs
     task(){
         echo "Running rules from $1 to $2"
         for i in $(eval echo {$1..$2});
         do
             ./MONAtoTruthTable.py ${input_dir}/rule${i}.out ${anml_dir}/rule${i}.tt
-            ./MONAtoTruthTable.py ${input_dir}/rule${i}.rout ${ranml_dir}/rule${i}.tt
+            ./MONAtoTruthTable.py ${input_dir}/rule${i}.rout ${ranml_dir}/rule${i}.tt --reverse
         done
     }
 
